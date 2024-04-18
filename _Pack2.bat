939251ac0f8bb2b52f3eccea285bf33bdef29314
Versusvs <Versusvs@users.noreply.github.com>
2024-04-18 23:09:32 +0300

    Add files via upload

---
+++ _Pack2.bat
@@ -0,0 +1,286 @@
+BMPTo3DOCel.exe A.bmp D0001.cel
+BMPTo3DOCel.exe B.bmp D0002.cel
+BMPTo3DOCel.exe C.bmp D0003.cel
+BMPTo3DOCel.exe D.bmp D0004.cel
+BMPTo3DOCel.exe E.bmp D0005.cel
+BMPTo3DOCel.exe F.bmp D0006.cel
+BMPTo3DOCel.exe G.bmp D0007.cel
+BMPTo3DOCel.exe H.bmp D0008.cel
+BMPTo3DOCel.exe I.bmp D0009.cel
+BMPTo3DOCel.exe J.bmp D0010.cel
+BMPTo3DOCel.exe K.bmp D0011.cel
+BMPTo3DOCel.exe L.bmp D0012.cel
+BMPTo3DOCel.exe M.bmp D0013.cel
+BMPTo3DOCel.exe N.bmp D0014.cel
+BMPTo3DOCel.exe O.bmp D0015.cel
+BMPTo3DOCel.exe P.bmp D0016.cel
+BMPTo3DOCel.exe Q.bmp D0017.cel
+BMPTo3DOCel.exe R.bmp D0018.cel
+BMPTo3DOCel.exe S.bmp D0019.cel
+BMPTo3DOCel.exe T.bmp D0020.cel
+BMPTo3DOCel.exe U.bmp D0021.cel
+BMPTo3DOCel.exe V.bmp D0022.cel
+BMPTo3DOCel.exe W.bmp D0023.cel
+BMPTo3DOCel.exe X.bmp D0024.cel
+BMPTo3DOCel.exe Y.bmp D0025.cel
+BMPTo3DOCel.exe Z.bmp D0026.cel
+
+BMPTo3DOCel.exe _A.bmp D0027.cel
+BMPTo3DOCel.exe _B.bmp D0028.cel
+BMPTo3DOCel.exe _C.bmp D0029.cel
+BMPTo3DOCel.exe _D.bmp D0030.cel
+BMPTo3DOCel.exe _E.bmp D0031.cel
+BMPTo3DOCel.exe _F.bmp D0032.cel
+BMPTo3DOCel.exe _G.bmp D0033.cel
+BMPTo3DOCel.exe _H.bmp D0034.cel
+BMPTo3DOCel.exe _I.bmp D0035.cel
+BMPTo3DOCel.exe _J.bmp D0036.cel
+BMPTo3DOCel.exe _K.bmp D0037.cel
+BMPTo3DOCel.exe _L.bmp D0038.cel
+BMPTo3DOCel.exe _M.bmp D0039.cel
+BMPTo3DOCel.exe _N.bmp D0040.cel
+BMPTo3DOCel.exe _O.bmp D0041.cel
+BMPTo3DOCel.exe _P.bmp D0042.cel
+BMPTo3DOCel.exe _Q.bmp D0043.cel
+BMPTo3DOCel.exe _R.bmp D0044.cel
+BMPTo3DOCel.exe _S.bmp D0045.cel
+BMPTo3DOCel.exe _T.bmp D0046.cel
+BMPTo3DOCel.exe _U.bmp D0047.cel
+BMPTo3DOCel.exe _V.bmp D0048.cel
+BMPTo3DOCel.exe _W.bmp D0049.cel
+BMPTo3DOCel.exe _X.bmp D0050.cel
+BMPTo3DOCel.exe _Y.bmp D0051.cel
+BMPTo3DOCel.exe _Z.bmp D0052.cel
+BMPTo3DOCel.exe ..bmp D0053.cel
+BMPTo3DOCel.exe !.bmp D0054.cel
+
+pause
+
+CELCutter003.exe D0001.CEL
+CELCutter003.exe D0002.CEL
+CELCutter003.exe D0003.CEL
+CELCutter003.exe D0004.CEL
+CELCutter003.exe D0005.CEL
+CELCutter003.exe D0006.CEL
+CELCutter003.exe D0007.CEL
+CELCutter003.exe D0008.CEL
+CELCutter003.exe D0009.CEL
+CELCutter003.exe D0010.CEL
+CELCutter003.exe D0011.CEL
+CELCutter003.exe D0012.CEL
+CELCutter003.exe D0013.CEL
+CELCutter003.exe D0014.CEL
+CELCutter003.exe D0015.CEL
+CELCutter003.exe D0016.CEL
+CELCutter003.exe D0017.CEL
+CELCutter003.exe D0018.CEL
+CELCutter003.exe D0019.CEL
+CELCutter003.exe D0020.CEL
+CELCutter003.exe D0021.CEL
+CELCutter003.exe D0022.CEL
+CELCutter003.exe D0023.CEL
+CELCutter003.exe D0024.CEL
+CELCutter003.exe D0025.CEL
+CELCutter003.exe D0026.CEL
+CELCutter003.exe D0027.CEL
+CELCutter003.exe D0028.CEL
+CELCutter003.exe D0029.CEL
+CELCutter003.exe D0030.CEL
+CELCutter003.exe D0031.CEL
+CELCutter003.exe D0032.CEL
+CELCutter003.exe D0033.CEL
+CELCutter003.exe D0034.CEL
+CELCutter003.exe D0035.CEL
+CELCutter003.exe D0036.CEL
+CELCutter003.exe D0037.CEL
+CELCutter003.exe D0038.CEL
+CELCutter003.exe D0039.CEL
+CELCutter003.exe D0040.CEL
+CELCutter003.exe D0041.CEL
+CELCutter003.exe D0042.CEL
+CELCutter003.exe D0043.CEL
+CELCutter003.exe D0044.CEL
+CELCutter003.exe D0045.CEL
+CELCutter003.exe D0046.CEL
+CELCutter003.exe D0047.CEL
+CELCutter003.exe D0048.CEL
+CELCutter003.exe D0049.CEL
+CELCutter003.exe D0050.CEL
+CELCutter003.exe D0051.CEL
+CELCutter003.exe D0052.CEL
+CELCutter003.exe D0053.CEL
+CELCutter003.exe D0054.CEL
+
+pause
+
+ren D0001.CEL.out D0001
+ren D0002.CEL.out D0002
+ren D0003.CEL.out D0003
+ren D0004.CEL.out D0004
+ren D0005.CEL.out D0005
+ren D0006.CEL.out D0006
+ren D0007.CEL.out D0007
+ren D0008.CEL.out D0008
+ren D0009.CEL.out D0009
+ren D0010.CEL.out D0010
+ren D0011.CEL.out D0011
+ren D0012.CEL.out D0012
+ren D0013.CEL.out D0013
+ren D0014.CEL.out D0014
+ren D0015.CEL.out D0015
+ren D0016.CEL.out D0016
+ren D0017.CEL.out D0017
+ren D0018.CEL.out D0018
+ren D0019.CEL.out D0019
+ren D0020.CEL.out D0020
+ren D0021.CEL.out D0021
+ren D0022.CEL.out D0022
+ren D0023.CEL.out D0023
+ren D0024.CEL.out D0024
+ren D0025.CEL.out D0025
+ren D0026.CEL.out D0026
+ren D0027.CEL.out D0027
+ren D0028.CEL.out D0028
+ren D0029.CEL.out D0029
+ren D0030.CEL.out D0030
+ren D0031.CEL.out D0031
+ren D0032.CEL.out D0032
+ren D0033.CEL.out D0033
+ren D0034.CEL.out D0034
+ren D0035.CEL.out D0035
+ren D0036.CEL.out D0036
+ren D0037.CEL.out D0037
+ren D0038.CEL.out D0038
+ren D0039.CEL.out D0039
+ren D0040.CEL.out D0040
+ren D0041.CEL.out D0041
+ren D0042.CEL.out D0042
+ren D0043.CEL.out D0043
+ren D0044.CEL.out D0044
+ren D0045.CEL.out D0045
+ren D0046.CEL.out D0046
+ren D0047.CEL.out D0047
+ren D0048.CEL.out D0048
+ren D0049.CEL.out D0049
+ren D0050.CEL.out D0050
+ren D0051.CEL.out D0051
+ren D0052.CEL.out D0052
+ren D0053.CEL.out D0053
+ren D0054.CEL.out D0054
+
+pause
+
+MakeFONT_VisualSt.exe
+
+del D0001
+del D0001.cel
+del D0002
+del D0002.cel
+del D0003
+del D0003.cel
+del D0004
+del D0004.cel
+del D0005
+del D0005.cel
+del D0006
+del D0006.cel
+del D0007
+del D0007.cel
+del D0008
+del D0008.cel
+del D0009
+del D0009.cel
+del D0010
+del D0010.cel
+del D0011
+del D0011.cel
+del D0012
+del D0012.cel
+del D0013
+del D0013.cel
+del D0014
+del D0014.cel
+del D0015
+del D0015.cel
+del D0016
+del D0016.cel
+del D0017
+del D0017.cel
+del D0018
+del D0018.cel
+del D0019
+del D0019.cel
+del D0020
+del D0020.cel
+del D0021
+del D0021.cel
+del D0022
+del D0022.cel
+del D0023
+del D0023.cel
+del D0024
+del D0024.cel
+del D0025
+del D0025.cel
+del D0026
+del D0026.cel
+del D0027
+del D0027.cel
+del D0028
+del D0028.cel
+del D0029
+del D0029.cel
+del D0030
+del D0030.cel
+del D0031
+del D0031.cel
+del D0032
+del D0032.cel
+del D0033
+del D0033.cel
+del D0034
+del D0034.cel
+del D0035
+del D0035.cel
+del D0036
+del D0036.cel
+del D0037
+del D0037.cel
+del D0038
+del D0038.cel
+del D0039
+del D0039.cel
+del D0040
+del D0040.cel
+del D0041
+del D0041.cel
+del D0042
+del D0042.cel
+del D0043
+del D0043.cel
+del D0044
+del D0044.cel
+del D0045
+del D0045.cel
+del D0046
+del D0046.cel
+del D0047
+del D0047.cel
+del D0048
+del D0048.cel
+del D0049
+del D0049.cel
+del D0050
+del D0050.cel
+del D0051
+del D0051.cel
+del D0052
+del D0052.cel
+del D0053
+del D0053.cel
+del D0054
+del D0054.cel
+
+ren SmallFont rSMALLCHARSET
+
+pause
