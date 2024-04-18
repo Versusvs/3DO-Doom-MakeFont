2093c13696199279984880df75dc8dc42cd767cb
Versusvs <Versusvs@users.noreply.github.com>
2024-04-18 23:08:03 +0300

    Add files via upload

---
+++ Small Font/_Pack.bat
@@ -0,0 +1,127 @@
+CELCutter003.exe STCFN033.8BIM.cel
+CELCutter003.exe STCFN048.8BIM.cel
+CELCutter003.exe STCFN049.8BIM.cel
+CELCutter003.exe STCFN050.8BIM.cel
+CELCutter003.exe STCFN051.8BIM.cel
+CELCutter003.exe STCFN052.8BIM.cel
+CELCutter003.exe STCFN053.8BIM.cel
+CELCutter003.exe STCFN054.8BIM.cel
+CELCutter003.exe STCFN055.8BIM.cel
+CELCutter003.exe STCFN056.8BIM.cel
+CELCutter003.exe STCFN057.8BIM.cel
+CELCutter003.exe STCFN058.8BIM.cel
+CELCutter003.exe STCFN065.8BIM.cel
+CELCutter003.exe STCFN066.8BIM.cel
+CELCutter003.exe STCFN067.8BIM.cel
+CELCutter003.exe STCFN068.8BIM.cel
+CELCutter003.exe STCFN069.8BIM.cel
+CELCutter003.exe STCFN070.8BIM.cel
+CELCutter003.exe STCFN071.8BIM.cel
+CELCutter003.exe STCFN072.8BIM.cel
+CELCutter003.exe STCFN073.8BIM.cel
+CELCutter003.exe STCFN074.8BIM.cel
+CELCutter003.exe STCFN075.8BIM.cel
+CELCutter003.exe STCFN076.8BIM.cel
+CELCutter003.exe STCFN077.8BIM.cel
+CELCutter003.exe STCFN078.8BIM.cel
+CELCutter003.exe STCFN079.8BIM.cel
+CELCutter003.exe STCFN080.8BIM.cel
+CELCutter003.exe STCFN081.8BIM.cel
+CELCutter003.exe STCFN082.8BIM.cel
+CELCutter003.exe STCFN083.8BIM.cel
+CELCutter003.exe STCFN084.8BIM.cel
+CELCutter003.exe STCFN085.8BIM.cel
+CELCutter003.exe STCFN086.8BIM.cel
+CELCutter003.exe STCFN087.8BIM.cel
+CELCutter003.exe STCFN088.8BIM.cel
+CELCutter003.exe STCFN089.8BIM.cel
+CELCutter003.exe STCFN090.8BIM.cel
+
+pause
+
+ren STCFN033.8BIM.cel.out !rez
+ren STCFN048.8BIM.cel.out 0rez
+ren STCFN049.8BIM.cel.out 1rez
+ren STCFN050.8BIM.cel.out 2rez
+ren STCFN051.8BIM.cel.out 3rez
+ren STCFN052.8BIM.cel.out 4rez
+ren STCFN053.8BIM.cel.out 5rez
+ren STCFN054.8BIM.cel.out 6rez
+ren STCFN055.8BIM.cel.out 7rez
+ren STCFN056.8BIM.cel.out 8rez
+ren STCFN057.8BIM.cel.out 9rez
+ren STCFN058.8BIM.cel.out .rez
+ren STCFN065.8BIM.cel.out arez
+ren STCFN066.8BIM.cel.out brez
+ren STCFN067.8BIM.cel.out crez
+ren STCFN068.8BIM.cel.out drez
+ren STCFN069.8BIM.cel.out erez
+ren STCFN070.8BIM.cel.out frez
+ren STCFN071.8BIM.cel.out grez
+ren STCFN072.8BIM.cel.out hrez
+ren STCFN073.8BIM.cel.out irez
+ren STCFN074.8BIM.cel.out jrez
+ren STCFN075.8BIM.cel.out krez
+ren STCFN076.8BIM.cel.out lrez
+ren STCFN077.8BIM.cel.out mrez
+ren STCFN078.8BIM.cel.out nrez
+ren STCFN079.8BIM.cel.out orez
+ren STCFN080.8BIM.cel.out prez
+ren STCFN081.8BIM.cel.out qrez
+ren STCFN082.8BIM.cel.out rrez
+ren STCFN083.8BIM.cel.out srez
+ren STCFN084.8BIM.cel.out trez
+ren STCFN085.8BIM.cel.out urez
+ren STCFN086.8BIM.cel.out vrez
+ren STCFN087.8BIM.cel.out wrez
+ren STCFN088.8BIM.cel.out xrez
+ren STCFN089.8BIM.cel.out yrez
+ren STCFN090.8BIM.cel.out zrez
+
+
+pause
+
+MakeFONT_VisualSt.exe
+
+del !rez
+del 0rez
+del 1rez
+del 2rez
+del 3rez
+del 4rez
+del 5rez
+del 6rez
+del 7rez
+del 8rez
+del 9rez
+del .rez
+del arez
+del brez
+del crez
+del drez
+del erez
+del frez
+del grez
+del hrez
+del irez
+del jrez
+del krez
+del lrez
+del mrez
+del nrez
+del orez
+del prez
+del qrez
+del rrez
+del srez
+del trez
+del urez
+del vrez
+del wrez
+del xrez
+del yrez
+del zrez
+
+ren SmallFont rSMALLCHARSET
+
+pause
